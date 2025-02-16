eb9a99c1e51c99f82487a522f59f7cc4e4d84ce8
dicefuji <fujiwaraadaisuke@gmail.com>
2025-02-16 15:34:33 -0800

    feat(layout): Reorganize dashboard components into horizontal layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainMenu from './components/MainMenu';
+import Welcome from './components/Welcome';
+import TaskList from './components/TaskList';
+import GamifiedProgress from './components/GamifiedProgress';
+import Leaderboard from './components/Leaderboard';
+import ProfileArea from './components/profile/ProfileArea';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainMenu />
+      <div className="main-content">
+        <Welcome />
+        <div className="dashboard">
+          <TaskList />
+          <GamifiedProgress />
+          <Leaderboard />
+          <ProfileArea />
+        </div>
+      </div>
     </div>
   );
 }
