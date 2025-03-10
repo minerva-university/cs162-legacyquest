b9be43580344f661e5e05bdfba2cc6e5aeef9d3a
dicefuji <fujiwaraadaisuke@gmail.com>
2025-03-09 19:23:40 -0700

    design changes

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -5,15 +5,32 @@ import UserProfile from '../components/UserProfile';
 export default function AdminPage() {
   return(
     <Stack direction='row' sx={{ height: '100%', alignItems: 'stretch'}}>
-      <Box sx={{flexGrow: 10}}>
+      <Box sx={{
+        flexGrow: 10,
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        overflow: 'hidden',
+        pr: 0
+      }}>
         <AdminContent />
       </Box>
       <Divider orientation='vertical' flexItem sx={{borderWidth: 1}}/>
 
       {/* User Profile on the right */}
-      <Box sx={{flexGrow: 1}}>
+      <Box sx={{
+        flexGrow: 1, 
+        display: 'flex', 
+        flexDirection: 'column', 
+        p: 4,
+        pt: 3,
+        pl: 3,
+        bgcolor: '#f9f9f9',
+        maxWidth: '350px',
+        overflow: 'auto'
+      }}>
         <UserProfile isAdmin={true} />
       </Box>
-  </Stack>
+    </Stack>
   )
 }
