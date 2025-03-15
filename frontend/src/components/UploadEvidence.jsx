ad0680dd50f887bbaa537784c4e91ae74d839b2e
Merxon22 <merxon22@gmail.com>
2025-03-15 13:31:54 -0300

    add drag-and-drop function to upload file evidence

--- frontend/src/components/UploadEvidence.jsx
+++ frontend/src/components/UploadEvidence.jsx
@@ -1,8 +1,9 @@
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, FormGroup, Typography, IconButton, Stack, Box } from '@mui/material';
+import { Dialog, DialogContent, DialogActions, Button, TextField, FormGroup, Typography, IconButton, Stack, Box } from '@mui/material';
 import { useState } from 'react';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import FileUpload from './FileUpload';
 
-export default function UploadEvidence({ open, onClose }) {
+export default function UploadEvidence({ open, onClose, taskName }) {
   const [description, setDescription] = useState('');
 
   const handleSubmit = (e) => {
@@ -13,13 +14,25 @@ export default function UploadEvidence({ open, onClose }) {
     onClose();
   };
 
+  const handleFileUpload = (files) => {
+    console.log('Uploaded Files:', files);
+  }
+
   return (
-    <Dialog open={open} onClose={onClose} maxWidth='sm' fullWidth>
-      <Stack sx={{p: 2}}>
+    <Dialog open={open} onClose={onClose} maxWidth='sm' fullWidth slotProps={{
+      paper: {
+        style: {
+          borderRadius: 16,
+          overflow: 'hidden'
+        }
+      }
+    }}>
+      <Stack sx={{p: 1}}>
         {/* Close Button */}
         <Stack direction='row' sx={{mb: 4}}>
           {/* Spacer */}
           <Box sx={{flexGrow: 1}} />
+
           <IconButton onClick={onClose}>
             <CloseRoundedIcon />
           </IconButton>
@@ -27,11 +40,15 @@ export default function UploadEvidence({ open, onClose }) {
         </Stack>
 
         {/* Form */}
-        <FormGroup onSubmit={handleSubmit}>
-          <Stack sx={{px: 10, textAlign: 'center'}}>
-            <Typography variant='h4' sx={{fontWeight: 800, mb: 2}}>Group Photo on Coit Tower</Typography>
+        <form onSubmit={handleSubmit}>
+          <Stack sx={{px: 4, pb: 2, textAlign: 'center'}}>
+            <Typography variant='h4' sx={{fontWeight: 800, mb: 2}}>Submit Evidence for {taskName}</Typography>
             <Typography>Enter Proof to submit and get points!</Typography>
             <DialogContent>
+              
+              {/* Upload file Button */}
+              <FileUpload onFileUpload={handleFileUpload}/>
+
               <TextField
                 margin='dense'
                 label='Description'
@@ -41,18 +58,21 @@ export default function UploadEvidence({ open, onClose }) {
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
                 required
+                sx={{mt: 2}}
               />
             </DialogContent>
             <DialogActions>
               <Button type='submit' variant='contained' fullWidth sx={{
                 background: 'linear-gradient(90deg, #020024 0%, #090979 20%, #00D4FF 100%)',
                 textTransform: 'none',
-                borderRadius: 2}}>
+                borderRadius: 2,
+                mx: 'auto',
+                maxWidth: '470px'}}>
                 Submit
               </Button>
             </DialogActions>
           </Stack>
-        </FormGroup>
+        </form>
       </Stack>
     </Dialog>
   );
