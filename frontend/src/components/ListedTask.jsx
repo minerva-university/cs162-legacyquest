228c80dee36e3e1e188b4722f51b85c78f3c4de5
Merxon22 <merxon22@gmail.com>
2025-04-03 21:57:24 -0300

    working progress on updated task display

--- frontend/src/components/ListedTask.jsx
+++ frontend/src/components/ListedTask.jsx
@@ -1,11 +1,28 @@
-import { Avatar, ListItem, Stack, Typography, Tooltip } from '@mui/material';
+import { Avatar, ListItem, Stack, Typography, Tooltip, Box } from '@mui/material';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import LinearProgress from './LinearProgress';
+import CancelIcon from '@mui/icons-material/Cancel';
 import UploadEvidence from './UploadEvidence';
 import { useState } from 'react';
 
-export default function ListedTask({taskName, currentProgress, targetProgress}) {
+export default function ListedTask({taskName, taskDescription, taskStatus}) {
   const [openDialog, setOpenDialog] = useState(false);
+
+  const getStatusColor = (status) => {
+    switch (status) {
+      case 'Not Submitted':
+        // return dark red
+        return '#b71c1c'; 
+      case 'Waiting Approval':
+        return 'orange';
+      case 'Approved':
+        return 'green';
+      case 'Rejected':
+        return 'red';
+      default:
+        return 'gray';
+    }
+  }
 
   return (
     <>
@@ -15,25 +32,18 @@ export default function ListedTask({taskName, currentProgress, targetProgress}) 
             }}} 
           onClick={() => setOpenDialog(true)}>
 
-          <Stack spacing={2} direction='row' sx={{alignItems: 'center', width: 1}}>
+          <Stack spacing={2} direction='row' sx={{alignItems: 'center', width: '100%', py: 0.5}}>
             {/* Task Avatar */}
-            <Avatar sx={{bgcolor: 'green', height: '48px'}} variant='rounded'>
+            <Avatar sx={{bgcolor: getStatusColor(taskStatus), flexShrink: 0}} variant='rounded'>
               <AssignmentIcon />
             </Avatar>
             
-            <Stack sx={{width: 1, alignItems: 'stretch'}}>
-              <Typography sx={{fontWeight: 600}}>{taskName}</Typography>
-              <Stack direction='row' sx={{alignItems: 'center'}}>
-                <LinearProgress
-                value={currentProgress / targetProgress * 100}
-                height={16}
-                barStartColor={'#FF9600'}
-                barEndColor={'#FFC800'}
-                backgroundColor='#E0E0E0'
-                borderRadius={4} />
-                <Typography sx={{fontWeight: 400, fontSize: 14, width: '50px', textAlign: 'center'}}>{currentProgress}/{targetProgress}</Typography>
-              </Stack>
+            <Stack sx={{minWidth: 0, flexGrow: 1, overflow: 'hidden'}}>
+              <Typography sx={{fontWeight: 600, textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'}}>{taskName}</Typography>
+              <Typography sx={{fontSize: 12, color: 'gray', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'}}>{taskDescription}</Typography>
             </Stack>
+            
+            <Typography sx={{fontWeight: 400, fontSize: 14, width: '60px', textAlign: 'center'}}>{taskStatus}</Typography>
           </Stack>
         </ListItem>
       </Tooltip>
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -3,19 +3,18 @@ import ListedTask from './ListedTask';
 
 export default function TaskList() {
   const tasks = [
-    {name: 'Task 1', currentProgress: 1, targetProgress: 7},
-    {name: 'Task 2', currentProgress: 3, targetProgress: 5},
-    {name: 'Task 3', currentProgress: 0, targetProgress: 10},
-    {name: 'Task 4', currentProgress: 2, targetProgress: 4},
-    {name: 'Task 5', currentProgress: 4, targetProgress: 6},
-    {name: 'Task 6', currentProgress: 1, targetProgress: 3},
+    {name: 'Task 1', description: 'A very long description. A very long description. A very long description. A very long description.', status: 'Not Submitted'},
+    {name: 'Task 2', description: 'Description 2..........', status: 'Not Submitted'},
+    {name: 'Task 3', description: 'Description 3..........', status: 'Waiting Approval'},
+    {name: 'Task 4', description: 'Description 4..........', status: 'Approved'},
+    {name: 'Task 5', description: 'Description 5..........', status: 'Rejected'},
   ]
 
   return (
-      <Stack sx={{width: 1, minWidth: '200px'}}>
+      <Stack sx={{width: 1, minWidth: '200px', maxWidth: '500px'}}>
         <List>
           {tasks.map((task, index) => (
-            <ListedTask key={index} taskName={task.name} currentProgress={task.currentProgress} targetProgress={task.targetProgress} />
+            <ListedTask key={index} taskName={task.name} taskDescription={task.description} taskStatus={task.status} />
           ))}
         </List>
       </Stack>
